f37e90185c2b5c4f7dd484df777c46deeb0f9419
seunghyun0522 <cvtcvt007@naver.com>
2023-10-10 13:22:15 +0900

    Youtube Frame 구현

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import Landing from "./components/Landing";
 import SignUp from "./components/SignUp";
 import Main from "./components/Main";
+import Lecture from "./components/Lecture";
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Landing />}></Route>
         <Route path="/Main" element={<Main />}></Route>
         <Route path="/SignUp" element={<SignUp />}></Route>
+        <Route path="/Lecture" element={<Lecture />}></Route>
       </Routes>
     </Router>
   );
