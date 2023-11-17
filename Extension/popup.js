ec0a4c938b61849a7f36f238b666a0a098609009
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2023-11-17 10:25:16 +0900

    feat: boj 소스 페이지 외 확장 프로그램 실행 시 예외 처리

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,27 +1,43 @@
 document.getElementById('copyButton').addEventListener('click', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    let tabId = tabs[0].id;
-    chrome.scripting.executeScript({
-      target: {tabId: tabId},
-      func: scrapeData
-    }, function(results) {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        return;
-      }
+    let currentTabId = tabs[0].id;
+    let tabUrl = tabs[0].url;
+    let targetUrl = "https://www.acmicpc.net/source/"
 
-      if (results && results[0]) {
-        chrome.storage.local.set({sourceData: results});
-      }
-    });
+    if (tabUrl.includes(targetUrl)){
+      chrome.scripting.executeScript({
+        target: {tabId: currentTabId},
+        func: scrapeData
+      }, function(results) {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
+        }
+
+        if (results && results[0]) {
+          chrome.storage.local.set({sourceData: results});
+        }
+      });
+    }
+
+    else {
+      alert("THIS EXTENSION ONLY WORKS ON BOJ SOURCE PAGES!!");
+    }
+    
   });
 });
   
 document.getElementById('pasteButton').addEventListener('click', function() {
   chrome.storage.local.get('sourceData', function(data) {
     if (data && data.sourceData) {
-      document.getElementById('dataDisplay').innerText = data.sourceData[0].result;
-      console.log(data.sourceData[0]);
+      if (data.sourceData[0].result[0]=='#'){
+        document.getElementById('dataDisplay').innerText = data.sourceData[0].result;
+        console.log(data.sourceData[0]);
+      }
+      else {
+        document.getElementById('dataDisplay').innerText = "숫자만 나옴!! DOM 확인해!!";
+      }
+      
     } else {
       document.getElementById('dataDisplay').innerText = "저장된 데이터가 없습니다.";
     }
@@ -32,7 +48,7 @@ function scrapeData() {
   let body = document.querySelector('body');
   let lines = body.innerText.split("\n");
   let filteredLines = [];
-  for (let i=10; i<=lines.length-72; i+=2){ //10일 떄가 있고, 3일 때가 있음...
+  for (let i=10; i<=lines.length-72; i+=2){ //숫자만 나오는 에러 생기면 여기 수정해야 함.
       filteredLines.push(lines[i]);
   }
   console.log(filteredLines);
