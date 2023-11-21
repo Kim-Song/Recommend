de17c8cc3c76096756f9ad6c779be9f12039fcde
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2023-11-21 18:18:50 +0900

    bugFix: 페이지 이동 후 코드를 읽어오지 못하는 오류 수정

--- Client/script/popup.js
+++ Client/script/popup.js
@@ -1,69 +1,64 @@
-document.getElementById('copyButton').addEventListener('click', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    let currentTabId = tabs[0].id;
-    let tabUrl = tabs[0].url;
-    let targetUrl = "https://www.acmicpc.net/status"
-    //https://www.acmicpc.net/status?from_mine=1&problem_id=13414&user_id=trankill1127
-    //좀 더 엄격하게 처리하면 좋겠지만?? 일단은 대충
+document.getElementById('analyzeButton').addEventListener('click', handleButtonClick);
 
-    if (tabUrl.includes(targetUrl)){
-      chrome.scripting.executeScript({
-        target: {tabId: currentTabId},
-        func: scrapeSubmitNumber
-      }, function(results){
-
-        let moveUrl = "https://www.acmicpc.net/source/"+results[0].result;
-        chrome.tabs.update({url:moveUrl});
-
-        chrome.scripting.executeScript({
-          target: {tabId: currentTabId},
-          func: scrapeData
-        }, function(results) {
-          if (results && results[0]) {
-            chrome.storage.local.set({sourceData: results});
-            showData();
+function handleButtonClick() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const currentTab = tabs[0];
+      const submitNumber = document.getElementById('submitNumber').value;
+      const onUpdatedCallback = function (tabId, info) {
+          if (info.status === 'complete') {
+              executeScriptOnTab(tabId, scrapeData, handleScriptResults);
+              chrome.tabs.onUpdated.removeListener(onUpdatedCallback);
           }
-        }); 
-      })
-    }
-    else {
-      alert("THIS EXTENSION ONLY WORKS ON BOJ MY SUBMIT PAGES!!")
-    }
+      };
+
+      chrome.tabs.onUpdated.addListener(onUpdatedCallback);
+      chrome.tabs.update({ url: "https://www.acmicpc.net/source/" + submitNumber });
   });
-});
+}
 
-function scrapeSubmitNumber(){
-  let body = document.querySelector('tbody');
-  let lines = body.innerText.split("\t");
-  let submitNumber = lines[0];
+function executeScriptOnTab(tabId, scriptFunction, callback) {
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: scriptFunction
+    }, callback);
+}
 
-  console.log(submitNumber);
+function handleScriptResults(results) {
+    if (chrome.runtime.lastError) {
+         console.error(chrome.runtime.lastError);
+         return;
+    }
 
-  return submitNumber;
+    if (results && results[0]) {
+        storeResultsInLocalStorage(results);
+        showData();
+    }
+}
+
+function storeResultsInLocalStorage(results) {
+    chrome.storage.local.set({ sourceData: results });
 }
 
 function scrapeData() {
-  let body = document.querySelector('tbody');
-  let lines = body.innerText.split("\n");
-  let filteredLines = [];
+    const body = document.querySelector('body');
+    const lines = body.innerText.split("\n");
+    const filteredLines = [];
 
-  for (let i=10; i<=lines.length-72; i+=2){ //숫자만 나오는 에러 생기면 여기 수정해야 함.
-    filteredLines.push(lines[i]);
-  }
-  return filteredLines.join("\n");
+    for (let i = 10; i <= lines.length - 72; i += 2) {
+        filteredLines.push(lines[i]);
+    }
+
+    return filteredLines.join("\n");
 }
 
-function showData(){
-  chrome.storage.local.get('sourceData', function(data) {
-    if (data && data.sourceData) {
-      if (data.sourceData[0].result[0]=='#'){
-        document.getElementById('dataDisplay').innerText = data.sourceData[0].result;
-      }
-      else {
-        document.getElementById('dataDisplay').innerText = "숫자만 나옴!! DOM 확인해!!";
-      }
-    } else {
-      document.getElementById('dataDisplay').innerText = "저장된 데이터가 없습니다.";
-    }
-  });
+function showData() {
+    chrome.storage.local.get('sourceData', function (data) {
+        const displayElement = document.getElementById('dataDisplay');
+        
+        if (data && data.sourceData) {
+            displayElement.innerText = data.sourceData[0].result[0] === '#' ? data.sourceData[0].result : "숫자만 나옴!! DOM 확인해!!";
+        } else {
+            displayElement.innerText = "저장된 데이터가 없습니다.";
+        }
+    });
 }
