b3ce506917756bd23d8cd0470a30f6819ff69b96
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2023-11-23 18:17:04 +0900

    chore: popup.js merge 오류 해결

--- Client/script/popup.js
+++ Client/script/popup.js
@@ -1,76 +1,58 @@
-document
-  .getElementById("analyzeButton")
-  .addEventListener("click", handleButtonClick);
+document.getElementById('analyzeButton').addEventListener('click', handleButtonClick);
 
 function handleButtonClick() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const currentTab = tabs[0];
-    const submitNumber = document.getElementById("submitNumber").value;
-    const onUpdatedCallback = function (tabId, info) {
-      if (info.status === "complete") {
-        executeScriptOnTab(tabId, scrapeData, handleScriptResults);
-        chrome.tabs.onUpdated.removeListener(onUpdatedCallback);
-      }
-    };
-
-    chrome.tabs.onUpdated.addListener(onUpdatedCallback);
-    chrome.tabs.update({
-      url: "https://www.acmicpc.net/source/" + submitNumber,
-    });
+      const submitNumber = document.getElementById('submitNumber').value;
+      const onUpdatedCallback = function (tabId, info) {
+          if (info.status === 'complete') {
+              executeScriptOnTab(tabId, scrapeCode, handleScriptResults);
+              chrome.tabs.onUpdated.removeListener(onUpdatedCallback);
+          }
+      };
+      chrome.tabs.onUpdated.addListener(onUpdatedCallback);
+      chrome.tabs.update({ url: "https://www.acmicpc.net/source/" + submitNumber });
   });
 }
 
 function executeScriptOnTab(tabId, scriptFunction, callback) {
-  chrome.scripting.executeScript(
-    {
-      target: { tabId: tabId },
-      func: scriptFunction,
-    },
-    callback
-  );
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: scriptFunction
+    }, callback);
+}
+
+function scrapeCode() {
+    const lines = document.querySelector('body').innerText.split("\n");
+    const codeLines = [];
+
+    if (lines[10].charAt(0)==="#"){
+        for (let i = 10; i <= lines.length - 72; i += 2) {
+            codeLines.push(lines[i]);
+        }
+        return codeLines.join("\n");
+    } else {
+        return "접근할 수 없는 제출 번호입니다.";
+    }
 }
 
 function handleScriptResults(results) {
-  if (chrome.runtime.lastError) {
-    console.error(chrome.runtime.lastError);
-    return;
-  }
-
-  if (results && results[0]) {
-    storeResultsInLocalStorage(results);
-    showData();
-  }
-}
-
-function storeResultsInLocalStorage(results) {
-  chrome.storage.local.set({ sourceData: results });
-}
-
-function scrapeData() {
-  const body = document.querySelector("body");
-  const lines = body.innerText.split("\n");
-  const filteredLines = [];
-
-  for (let i = 10; i <= lines.length - 72; i += 2) {
-    filteredLines.push(lines[i]);
-  }
-
-  return filteredLines.join("\n");
-}
-
-function showData() {
-  chrome.storage.local.get("sourceData", function (data) {
-    const displayElement = document.getElementById("dataDisplay");
-
-    if (data && data.sourceData) {
-      const displayText =
-        data.sourceData[0].result[0] === "#"
-          ? data.sourceData[0].result
-          : "숫자만 나옴!! DOM 확인해!!";
-      displayElement.innerText = displayText;
-    } else {
-      displayElement.innerText = "저장된 데이터가 없습니다.";
+    if (chrome.runtime.lastError) {
+         console.error(chrome.runtime.lastError);
+         return;
     }
-    displayElement.classList.add("styled-text");
-  });
+
+    if (results && results[0]) {
+        chrome.storage.local.set({ sourceData: results[0].result });
+        showCode();
+    }
+}
+
+function showCode() {
+    chrome.storage.local.get('sourceData', function (data) {
+        const displayElement = document.getElementById('dataDisplay');
+        if (data && data.sourceData) {
+            displayElement.innerText=data.sourceData;
+        } 
+        displayElement.classList.add('styled-text');
+    });
 }
